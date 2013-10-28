fc0f23d670fad614f59ff1cecdc09a8e2425d3ee
1000ch <orz1000ch@gmail.com>
2013-10-28 23:08:44 +0900

    update script

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,19 @@
 (function() {
 
   chrome.browserAction.onClicked.addListener(function(tab) {
-    
+    if(!tab) {
+      return;
+    }
+
+    chrome.storage.local.set({
+      currentUrl: tab.url,
+      currentTitle: tab.title
+    }, function() {
+      chrome.browserAction.setPopup({
+        tabId: tab.id,
+        popup: "/src/html/popup.html"
+      });
+    });
   });
 
 })();
---
+++ src/js/popup.js
@@ -0,0 +1,17 @@
+(function() {
+  chrome.storage.local.get(["currentUrl", "currentTitle"], function(items) {
+    console.log(items);
+    var params = [];
+    params.push("url=" + encodeURIComponent(items.currentUrl));
+    params.push("title=" + encodeURIComponent(items.currentTitle));
+    //params.push("description=" + encodeURIComponent(description));
+
+    var pinboardFrame = document.createElement("iframe");
+    pinboardFrame.width = "100%";
+    pinboardFrame.height = "100%";
+    pinboardFrame.src = "https://pinboard.in/add?" + params.join("&");
+
+    document.body.innerHTML = "";
+    document.body.appendChild(pinboardFrame);
+  });
+})();
