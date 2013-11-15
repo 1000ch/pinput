0e7a1c7be7d7712b75ce37303b05a3aa6da6e954
1000ch <orz1000ch@gmail.com>
2013-11-15 22:07:57 +0900

    Little fix. Thanks to @pocotan001 (๑´ڡ`๑)

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,8 +2,9 @@
 // https://code.google.com/p/chromium/issues/detail?id=111660#c7
 if(location.search !== "?foo") {
   location.search = "?foo";
-  throw new Error; // load everything on the next page;
-                   // stop execution on this page
+  throw new Error();
+  // load everything on the next page;
+  // stop execution on this page
 }
 
 $(function() {
@@ -34,48 +35,114 @@ $(function() {
     failed: "Bookmark is failed..."
   };
 
+  /**
+   * Split string with whitespace
+   * @param {String} val
+   * @returns {String}
+   */
   function split(val) {
     return val.split(/ \s*/);
   }
+
+  /**
+   * Extract last
+   * @param {String} term
+   * @returns {String}
+   */
   function extractLast(term) {
     return split(term).pop();
+  }
+
+  /**
+   * Serialize object to array
+   * @param {Object} param
+   * @returns {Array}
+   */
+  function serializeArray(param) {
+    // array to return
+    var array = [];
+    // fix argument
+    param = param || {};
+    
+    Object.keys(param).forEach(function(key) {
+      array.push(key + "=" + param[key]);
+    });
+    
+    return array;
+  }
+
+  /**
+   * Bookmark and other process
+   */
+  function bookmark() {
+    // create query string
+    var params = serializeArray({
+      format: "json",
+      auth_token: token,
+      url: encodeURIComponent($url.val()),
+      description: encodeURIComponent($title.val()),
+      extended: encodeURIComponent($description.val()),
+      tags: encodeURIComponent($tags.val()),
+      shared: ($private.prop("checked") ? "no" : "yes"),
+      toread: ($readlater.prop("checked") ? "yes" : "no"),
+      _: Date.now()
+    });
+
+    var $jqxhrAdd = $.getJSON("https://api.pinboard.in/v1/posts/add?" + params.join("&"));
+    $jqxhrAdd.done(function(data) {
+      if(data.result_code !== "done") {
+        $alert.removeClass("alert-info alert-warning alert-success");
+        $alert.html(data.result_code).addClass("alert-danger");
+      } else {
+        $alert.removeClass("alert-info alert-warning alert-danger");
+        $alert.html(resultMessage.succeed).addClass("alert-success");
+
+        // close popup window
+        window.setTimeout(function() {
+          window.close();
+        }, 500);
+      }
+    }).fail(function(error) {
+      $alert.removeClass("alert-info alert-warning alert-success");
+      $alert.html(error).addClass("alert-danger");
+    });
   }
 
   // when popup is opened,
   // send blank message to background
   chrome.runtime.sendMessage({}, function(response) {
-
+    // get url and title of current tab
     $url.val(response.url);
     $title.val(response.title);
+    
+    // focus to tag area
     $tags.focus();
 
     // get Token and check
     chromeStorage.get([storageKey.APIToken, storageKey.isAuthenticated], function(item) {
-
+      // cache token
       token = item[storageKey.APIToken];
 
       if(!item[storageKey.isAuthenticated]) {
-
         // if API token is not authenticated, make me disabled.
         $alert.removeClass("alert-info alert-warning alert-success");
         $alert.html(resultMessage.isNotAuthenticated).addClass("alert-danger");
         $bookmark.attr("disabled", "disabled");
-
       } else {
-
         // create parameters
-        var params = [];
-        params.push("format=json");
-        params.push("auth_token=" + token);
-        params.push("url=" + response.url);
-        params.push("_=" + Date.now());
+        var params = serializeArray({
+          format: "json",
+          auth_token: token,
+          url: response.url,
+          _: Date.now()
+        });
 
         // check whether url is bookmarked or not
         var $jqxhrGet = $.getJSON("https://api.pinboard.in/v1/posts/get?" + params.join("&"));
         $jqxhrGet.done(function(data) {
           if(data.posts.length !== 0) {
             // if url is already bookmarked
-            $tags.val(data.posts[0].tags);
+            $tags.val(data.posts.shift().tags);
             $alert.removeClass("alert-info alert-success alert-danger");
             $alert.html(resultMessage.isBookmarked).addClass("alert-warning");
           } else {
@@ -94,8 +161,8 @@ $(function() {
           $.getJSON("https://api.pinboard.in/v1/tags/get?format=json&auth_token=" + token).done(function(data) {
 
             var availableTags = Object.keys(data);
-            $tags.bind("keydown", function( event ) {
-              if(event.keyCode === $.ui.keyCode.TAB && $(this).data( "ui-autocomplete" ).menu.active) {
+            $tags.bind("keydown", function(event) {
+              if(event.keyCode === $.ui.keyCode.TAB && $(this).data("ui-autocomplete").menu.active) {
                 event.preventDefault();
               }
             }).autocomplete({
@@ -115,9 +182,9 @@ $(function() {
                 // remove the current input
                 terms.pop();
                 // add the selected item
-                terms.push( ui.item.value );
+                terms.push(ui.item.value);
                 // add placeholder to get the comma-and-space at the end
-                terms.push( "" );
+                terms.push("");
                 this.value = terms.join(" ");
                 return false;
               }
@@ -125,45 +192,19 @@ $(function() {
           });
         });
 
-        function doBookmark(e) {
-
+        $form.on("submit", function(e) {
           // prevent default
           e.preventDefault();
 
-          // create query string
-          var params = [];
-          params.push("format=json");
-          params.push("auth_token=" + token);
-          params.push("url=" + encodeURIComponent($url.val()));
-          params.push("description=" + encodeURIComponent($title.val()));
-          params.push("extended=" + encodeURIComponent($description.val()));
-          params.push("tags=" + encodeURIComponent($tags.val()));
-          params.push("shared=" + ($private.prop("checked") ? "no" : "yes"));
-          params.push("toread=" + ($readlater.prop("checked") ? "yes" : "no"));
-          params.push("_=" + Date.now());
+          bookmark();
+        });
 
-          var $jqxhrAdd = $.getJSON("https://api.pinboard.in/v1/posts/add?" + params.join("&"));
-          $jqxhrAdd.done(function(data) {
-            if(data.result_code !== "done") {
-              $alert.removeClass("alert-info alert-warning alert-success");
-              $alert.html(data.result_code).addClass("alert-danger");
-            } else {
-              $alert.removeClass("alert-info alert-warning alert-danger");
-              $alert.html(resultMessage.succeed).addClass("alert-success");
-
-              // close popup window
-              window.setTimeout(function() {
-                window.close();
-              }, 500);
-            }
-          }).fail(function(error) {
-            $alert.removeClass("alert-info alert-warning alert-success");
-            $alert.html(error).addClass("alert-danger");
-          });
-        }
-
-        $form.on("submit", doBookmark);
-        $bookmark.on("click", doBookmark);
+        $bookmark.on("click", function(e) {
+          // prevent default
+          e.preventDefault();
+          
+          bookmark();
+        });
       }
     });
   });
