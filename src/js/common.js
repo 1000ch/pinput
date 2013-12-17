d43108866037637635e35863d502eca59a39f6b3
1000ch <orz1000ch@gmail.com>
2013-12-17 20:05:47 +0900

    add comments

--- src/js/common.js
+++ src/js/common.js
@@ -39,13 +39,24 @@
       return array;
     }
   };
-  
+
   Pinput.StorageKey = {
     authToken: "pinput_authToken",
     isAuthenticated: "pinput_isAuthenticated"
   };
   
   Pinput.API = {
+    /**
+     * Add a bookmark.
+     * @see https://pinboard.in/api#posts_add
+     * @param {String} url [required]
+     * @param {String} title [required]
+     * @param {String} description
+     * @param {String} tags
+     * @param {String} shared
+     * @param {String} toread
+     * @returns {$.Deferred}
+     */
     addPost: function(url, title, description, tags, shared, toread) {
       var params = Pinput.Util.serializeArray({
         format: "json",
@@ -61,6 +72,13 @@
       // all of API is get method
       return $.getJSON('https://api.pinboard.in/v1/posts/add?' + params.join('&'));
     },
+    /**
+     * Returns one or more posts on a single day matching the arguments.
+     * If no date or url is given, date of most recent bookmark will be used.
+     * @see https://pinboard.in/api#posts_get
+     * @param {String} url
+     * @returns {$.Deferred}
+     */
     getPost: function(url) {
       var params = Pinput.Util.serializeArray({
         format: "json",
@@ -71,6 +89,14 @@
       // all of API is get method
       return $.getJSON('https://api.pinboard.in/v1/posts/get?' + params.join('&'));
     },
+    /**
+     * Returns a list of popular tags and recommended tags for a given URL.
+     * Popular tags are tags used site-wide for the url;
+     * recommended tags are drawn from the user's own tags.
+     * @see https://pinboard.in/api#posts_suggest
+     * @param {String} url
+     * @returns {$.Deferred}
+     */
     suggestPost: function(url) {
       var params = Pinput.Util.serializeArray({
         format: "json",
@@ -81,6 +107,11 @@
       // all of API is get method
       return $.getJSON('https://api.pinboard.in/v1/posts/suggest?' + params.join('&'));
     },
+    /**
+     * Returns a full list of the user's tags along with the number of times they were used.
+     * @see https://pinboard.in/api#tags_get
+     * @returns {$.Deferred}
+     */
     getTags: function() {
       var params = Pinput.Util.serializeArray({
         format: "json",
