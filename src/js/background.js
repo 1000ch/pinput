21085c7cf04c9924c03e89baa59e050112a4bf0b
1000ch <shogo.sensui@gmail.com>
2016-01-13 23:58:24 +0900

    tweaks
    - fix description
    - specify shortcut `command`
    - fix path to html

--- src/js/background.js
+++ src/js/background.js
@@ -218,7 +218,7 @@ chrome.commands.onCommand.addListener(command => {
 chrome.runtime.onInstalled.addListener(details => {
   if (details.reason === 'install') {
     chrome.tabs.create({
-      url : '/html/options.html'
+      url : 'html/options.html'
     });
   }
 });
