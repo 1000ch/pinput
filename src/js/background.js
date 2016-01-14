662c663e2991d2514052153642c5c8ed686b492d
1000ch <shogo.sensui@gmail.com>
2016-01-14 12:39:40 +0900

    fix errors

--- src/js/background.js
+++ src/js/background.js
@@ -222,14 +222,3 @@ chrome.runtime.onInstalled.addListener(details => {
     });
   }
 });
-
-// add open pinboard to browser action right click menu
-chrome.contextMenus.create({
-  title    : 'Open Pinboard',
-  contexts : ['browser_action'],
-  onclick  : () => {
-    chrome.tabs.create({
-      url : 'https://pinboard.in/'
-    });
-  }
-});
